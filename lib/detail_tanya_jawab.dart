860296653579ab0a6da5e35d9faa0f20dc3ca0a0
yunaandreina26 <yunaandreina99@gmail.com>
2021-01-28 10:34:23 +0700

    menambahkan desain forum tanya jawab

---
+++ lib/detail_tanya_jawab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailTanyaJawab extends StatefulWidget {
+  @override
+  _DetailTanyaJawabState createState() => _DetailTanyaJawabState();
+}
+
+class _DetailTanyaJawabState extends State<DetailTanyaJawab> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/listquest_screen.dart
+++ lib/listquest_screen.dart
@@ -10,22 +10,116 @@ class ListquestScreen extends StatefulWidget {
 class _ListquestScreenState extends State<ListquestScreen> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Servicefstore.getThread(),
-      builder: (context, snapshot) {
-      if (snapshot.hasData) {
-        var list = snapshot.data.docs;
-        return ListView.builder(
-          itemCount: list.length,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Forum Tanya Jawab",
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      backgroundColor: Colors.grey[100],
+      body: Container(
+        child: ListView.builder(
+          itemCount: 10,
           itemBuilder: (context, index) {
-            return Text(list[index].data()['thread'],);
+            return Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Title",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Description",
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      DateTime.now().toIso8601String(),
+                    ),
+                  ),
+                  Divider(),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.favorite_border,
+                              size: 20,
+                            ),
+                            SizedBox(width: 5),
+                            Text("Like"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.message_outlined,
+                              size: 20,
+                            ),
+                            SizedBox(width: 5),
+                            Text("Comment"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.share,
+                              size: 20,
+                            ),
+                            SizedBox(width: 5),
+                            Text("Share"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
           },
-        );
-      }
-      return Container(
-        alignment: Alignment.center,
-        child: CircularProgressIndicator(),
-      );
-    });
+        ),
+      ),
+    );
+    // return StreamBuilder<QuerySnapshot>(
+    //   stream: Servicefstore.getThread(),
+    //   builder: (context, snapshot) {
+    //   if (snapshot.hasData) {
+    //     var list = snapshot.data.docs;
+    //     return ListView.builder(
+    //       itemCount: list.length,
+    //       itemBuilder: (context, index) {
+    //         return Text(list[index].data()['thread'],);
+    //       },
+    //     );
+    //   }
+    //   return Container(
+    //     alignment: Alignment.center,
+    //     child: CircularProgressIndicator(),
+    //   );
+    // });
   }
 }
